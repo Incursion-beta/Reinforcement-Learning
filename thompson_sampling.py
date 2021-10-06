3ebaffd5aff21532fa0579cb7b3e3fbd9df2e2a1
Aman Kumar Maurya <amanrkt231217@gmail.com>
2021-10-06 11:25:26 +0530

    Update thompson_sampling.py

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -5,16 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Implementing Thompson Sampling
+import random
+
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
-# Implementing Thompson Sampling
-import random
 N = 10000
 d = 10
-ads_selected = []
-numbers_of_rewards_1 = [0] * d
-numbers_of_rewards_0 = [0] * d
+all_ads_selected = []
+numbers_of_all_rewards_1 = [0] * d
+numbers_of_all_rewards_0 = [0] * d
 total_reward = 0
 
 for n in range(0, N):
@@ -22,7 +23,7 @@ for n in range(0, N):
     max_random = 0
     
     for i in range(0, d):
-        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)        
+        random_beta = random.betavariate(numbers_of_all_rewards_1[i] + 1, numbers_of_all_rewards_0[i] + 1)        
         if random_beta > max_random:
             max_random = random_beta
             ad = i
@@ -31,14 +32,18 @@ for n in range(0, N):
     reward = dataset.values[n, ad]
     
     if reward == 1:
-        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+        numbers_of_all_rewards_1[ad] = numbers_of_all_rewards_1[ad] + 1
     else:
-        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+        numbers_of_all_rewards_0[ad] = numbers_of_all_rewards_0[ad] + 1
     total_reward = total_reward + reward
 
 # Visualising the results - Histogram
 plt.hist(ads_selected)
+
 plt.title('Histogram of ADs selections')
+
 plt.xlabel('ADs')
+
 plt.ylabel('Number of times each Ad was selected')
+
 plt.show()
